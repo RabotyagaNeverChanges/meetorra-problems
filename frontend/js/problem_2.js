8fce3cc20cf7f7518ccf22ae713d172c1bfb8013
Kharlamov Vladislav <kharlamov.workspace@gmail.com>
2022-10-27 19:20:55 +0300

    Solution for problem #3

--- frontend/js/problem_2.js
+++ frontend/js/problem_2.js
@@ -1,11 +1,11 @@
-let menuLinks = document.querySelectorAll(".menu-list__link");
+let menuLinks = document.querySelectorAll(".menu-list__item");
 
 menuLinks.forEach(element => {
     element.addEventListener("click", event => {
-        let previousActiveElements = document.querySelectorAll(".menu-list__active-link");
+        let previousActiveElements = document.querySelectorAll(".menu-list__active-item");
         previousActiveElements.forEach(element => {
-           element.classList.remove("menu-list__active-link");
+           element.classList.remove("menu-list__active-item");
         });
-        event.target.classList.add("menu-list__active-link");
+        event.target.classList.add("menu-list__active-item");
     });
 })
